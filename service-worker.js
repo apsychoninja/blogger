d1f66c4f0428870681e33127a8efdf6d2fb36376
apsychoninja <50403520+apsychoninja@users.noreply.github.com>
2020-08-21 10:00:06 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,34 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
 
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
 // CODELAB: Add list of files to cache here.
 // const FILES_TO_CACHE = [
 //   'https://cdn.jsdelivr.net/gh/googlecodelabs/your-first-pwapp@master/public/offline.html',
